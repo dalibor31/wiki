2c3744ea98efb433679402ef4734787042a2324f
Dalibor Marković <dalibor31@gmail.com>
2026-06-14 23:38:58 +0200

    fix: migrate passport-saml to @node-saml/passport-saml 5.x (critical CVE)
    passport-saml 3.x has a critical CVE allowing SAML signature verification bypass.
    @node-saml/passport-saml is the official successor package.
    
    - Replace package in package.json (3.2.4 → 5.1.0)
    - Remove xml-crypto resolution workaround (no longer needed)
    - Update require in saml/authentication.js
    - Rename cert option to idpCert (breaking change since v4+)

--- server/modules/authentication/saml/authentication.js
+++ server/modules/authentication/saml/authentication.js
@@ -6,7 +6,7 @@ const _ = require('lodash')
 // SAML Account
 // ------------------------------------
 
-const SAMLStrategy = require('passport-saml').Strategy
+const SAMLStrategy = require('@node-saml/passport-saml').Strategy
 
 module.exports = {
   init (passport, conf) {
@@ -14,7 +14,7 @@ module.exports = {
       callbackUrl: conf.callbackURL,
       entryPoint: conf.entryPoint,
       issuer: conf.issuer,
-      cert: (conf.cert || '').split('|'),
+      idpCert: (conf.cert || '').split('|'),
       signatureAlgorithm: conf.signatureAlgorithm,
       digestAlgorithm: conf.digestAlgorithm,
       identifierFormat: conf.identifierFormat,
